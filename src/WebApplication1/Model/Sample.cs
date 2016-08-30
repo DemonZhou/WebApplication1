d97ea123d49cccbaa038b72eb839bbc130f1cd46
ZhouZhou` <ZhouZhou`@DESKTOP-49RT8H4>
2016-08-31 00:51:09 +0800

    DotNetCore

---
+++ src/WebApplication1/Model/Sample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Model
+{
+    public class Sample
+    {
+        public int id { get; set; }
+        public int value { get; set; }
+    }
+}
